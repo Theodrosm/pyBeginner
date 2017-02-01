2c17cacec799d2214bdba3ef82160b5c453c3324
Theodros Haile <Station@Station-Landing.local>
2017-02-01 17:59:22 -0500

    changed ans

--- ex06.py
+++ ex06.py
@@ -1,9 +1,7 @@
 # Exercise 6
 ## Objective: using the function combine(word1, word2) from ex5.py, create a function counter(word) that will return the number of characters in the portmanteau
 
-def combine(word1,word2):
-        return word1 +word2
-
+from ex05 import combine 
 
 
 def counter(word):
