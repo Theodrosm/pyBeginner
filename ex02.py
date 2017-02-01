d0634765f5c850545997281dcadeb4c88320a14b
Theodros Haile <Station@Station-Landing.local>
2017-02-01 17:51:20 -0500

    hlp

--- ex02.py
+++ ex02.py
@@ -1,2 +1,4 @@
 # Exercise 2
 ## Objective: place all letters of "codeNIH is the best!" in an array AND print array elements 14 to 19
+CDNH=["codeNIH is the best!"]
+print(CDNH[0][14:19])
--- ex03.py
+++ ex03.py
@@ -1,2 +1,4 @@
 # Exercise 3
 ## Objective: prompt a user to enter their age; script should output "Your age is [age here]"
+age = input("How old are you? :")
+print("your age is " +age)
